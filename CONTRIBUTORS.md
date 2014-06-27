caf67147c7215ac435e829b7dd6beb865ee19670
zhixin <wenzhixin2010@gmail.com>
2014-06-27 09:32:44 +0800

    Add contributors

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,18 @@
+## Contributors
+
+Multiple select is due to the excellent work of the following contributors:
+
+<table>
+<tr>
+<th>Author</th>
+<th>Github</th>
+<th>Email</th>
+<th>Blog</th>
+<th>Commits</th>
+</tr>
+
+%s
+
+Update date: %s
+
+</table>
---
+++ contributors.js
@@ -0,0 +1,65 @@
+var fs = require('fs'),
+    request = require('request'),
+    async = require('async'),
+    sprintf = require('sprintf'),
+    moment = require('moment'),
+    util = require('util'),
+
+    USER = 'wenzhixin';
+    REPO = 'multiple-select';
+
+request(getOptions(sprintf('https://api.github.com/repos/%s/%s/stats/contributors', USER, REPO)), function(err, res, body) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    var contents = [],
+        contributors = JSON.parse(body);
+
+    if (!util.isArray(contributors)) {
+        console.log('Get contributors error: ' + body);
+        return;   
+    }
+
+    contributors = contributors.sort(function(a, b) {
+        if (a.total > b.total) return -1;
+        if (a.total < b.total) return 1;
+        return 0;
+    });
+    async.eachSeries(contributors, function(contributor, callback) {
+        request(getOptions('https://api.github.com/users/' + contributor.author.login), function(err, res, body) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            var user = JSON.parse(body);
+            contents.push(
+                '<tr>',
+                '<td>' + (user.name || contributor.author.login) + '</td>',
+                '<td><a href="' + contributor.author.html_url + '">Github/' + contributor.author.login + '</a></td>',
+                '<td>' + (user.email ? '<a href="mailto:' + user.email + '">' + user.email + '</a>' : '') + '</td>',
+                '<td>' + (user.blog ? '<a herf="' + user.blog + '">' + user.blog + '</a>' : '') + '</td>',
+                '<td>' + contributor.total + '</td>',
+                '</tr>'
+            );
+            console.log(sprintf('Add %s: OK...', contributor.author.login));
+            callback();
+        });
+    }, function() {
+        var template = fs.readFileSync('./CONTRIBUTORS.md').toString(),
+            date = new Date();
+
+        fs.writeFileSync(sprintf('%s-%s-CONTRIBUTORS.md', USER, REPO),
+            sprintf(template, contents.join('\n'), moment(new Date()).format('YYYY-MM-DD')));
+        console.log('OK...');
+    });
+});
+
+function getOptions(url) {
+    return {
+        url: url,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36'
+        }
+    };
+}
